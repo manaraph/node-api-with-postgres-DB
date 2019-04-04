bd70adb0672f4e04a0d12dc715e4dc0a1697671a
Manasseh Omachonu <manaraph225@gmail.com>
2019-04-04 20:36:33 +0100

    Call request methods

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const db = require('./queries')
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
@@ -13,6 +14,12 @@ app.use(
 app.get('/', (request, response) => {
     response.json({info: 'Node.js, Express, and Postgres API'})
 })
+
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUsersByID)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
 
 app.listen(port, () => {
     console.log(`App running on port ${port}`)
